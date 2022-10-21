2297d7b66658664b8729e89a3c4d98c5d4afe9d3
Lucia Meyer <luciameyer1603@gmail.com>
2022-10-20 22:00:05 -0300

    #4

--- adventJS/#2_listGifts.js
+++ adventJS/#2_listGifts.js
@@ -1,12 +1,12 @@
+// https://adventjs.dev/
+
 /* ---------->>> Adventjs #02.js <<<----------
 
 Te ha llegado una carta ✉️ con todos los regalos que debes preparar. El tema es que es una cadena de texto y es muy difícil de leer 😱. ¡Menos mal que han puesto cada regalo separado por espacio! (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
-
 Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
-
 Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto: */
 
-const carta = 'bici coche balón _playstation bici coche peluche'
+const letter = 'bici coche balón _playstation bici coche peluche'
 
 /* Al ejecutar el método debería devolver lo siguiente:
 const regalos = listGifts(carta)
@@ -15,14 +15,13 @@ console.log(regalos)
 Ten en cuenta que los tests pueden ser más exhaustivos... 😝 ¡Cuidado con contar espacios vacíos! */
 
 const listGifts = letter => { 
-  let cleanArr = letter.split(' ')
+  let obj = letter.split(' ')
   .filter(l => l !== '' && l.charAt(0) !== '_')
   .reduce((count, e) => {
     count[e] = (count[e] || 0) + 1;
     return count
     }, {});
-  return cleanArr
+  return obj
 }
 
-const regalos = listGifts(carta)
-console.log(regalos)
+console.log(listGifts(letter))
--- adventJS/#3_isValid.js
+++ adventJS/#3_isValid.js
@@ -1,13 +1,11 @@
+// https://adventjs.dev/
+
 /* ---------->>> Adventjs #03.js <<<----------
 
 El Grinch está abriendo las cartas que iban a Santa Claus y las está dejando hechas un lío. 😱
-
 Las cartas son una cadena de texto que incluyen regalos y paréntesis ().
-
 Para saber si una carta es válida ✅, debes comprobar que los paréntesis cierran correctamente y que, además, no vayan vacíos.
-
 ¡Pero ojo! Porque el Grinch ha dejado llaves { y corchetes [ dentro de los paréntesis que hacen que no sean válidas. Por suerte sólo los ha dejado en medio de los paréntesis...
-
 Ejemplos: */
 
 "bici coche (balón) bici coche peluche" // -> ✅
@@ -21,6 +19,22 @@ Ejemplos: */
 /* Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch! */
 
 const isValid = letter => {
-  // ¡No dejes que el Grinch gane!
-  return true
- }
+  if(letter.includes('[') || letter.includes(']') || letter.includes('{') || letter.includes('}') || letter.includes('()')) return false;
+  let arr = letter.split(' ');
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i].charAt(0) === ')' || arr[i].charAt(arr[i].length -1) === '(') return false;
+    if(arr[i].charAt(0) === '(' && arr[i].charAt(arr[i].length -1) !== ')') return false;
+    if(arr[i].charAt(0) === '(' && arr[i].charAt(arr[i].length -1) === ')' && arr[i].length > 2 ) return true;
+  }
+  return true;
+};
+
+console.log(isValid("bici coche (balón) bici coche peluche")) 
+console.log(isValid("(muñeca) consola bici")) 
+console.log(isValid("bici coche (balón bici coche")) 
+console.log(isValid("peluche (bici [coche) bici coche balón")) 
+console.log(isValid("(peluche {) bici")) 
+console.log(isValid(")bici( casa play")) 
+console.log(isValid("() bici")) 
+console.log(isValid("(muñeca) consola bici")) 
+
