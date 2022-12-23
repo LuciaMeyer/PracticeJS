8014c3cbf960b501d744b9760a9e369bc884cefb
LuciaM <luciameyer1603gmail.com>
2022-12-23 12:34:30 -0300

    challenge03

--- codember/challenge03/challenge03.js
+++ codember/challenge03/challenge03.js
@@ -1,9 +1,28 @@
 import axios from 'axios';
 
-const coloredZebra = async () => {
+const lights = async () => {
 
-    const chrisLights = (await axios.get('https://codember.dev/colors.txt')).data;
-    console.log(chrisLights);
+    const colors = (await axios.get('https://codember.dev/colors.txt')).data;
+    // console.log(colors)
+    
+    let maxLength = 0
+    let maxLastColor = ''
+    let lastColor = ''
+    let nextColor = colors[0]
+    let currentLength = 0
+
+    colors.forEach(color => {
+        if (color === lastColor) return currentLength = 0
+        if (color !== nextColor || lastColor === color) currentLength = 1
+        currentLength++
+        [lastColor, nextColor] = [color, lastColor]
+        if (currentLength >= maxLength) {
+            maxLength = currentLength
+            maxLastColor = lastColor
+        }
+    })
+    console.log(`${maxLength}@${maxLastColor}`)
 };
 
-coloredZebra();
+lights();
+
