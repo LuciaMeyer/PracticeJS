639e27ecdd33b46a2fb7a395bc6ce5bd73d977a7
LuciaMeyer <lucia@codice.dev>
2023-11-25 18:59:40 -0300

    starting the pokemon app

---
+++ Vue/03-pokemon-game/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <PokemonPage/>
+  </div>
+</template>
+
+<script>
+import PokemonPage from './pages/PokemonPage.vue';
+export default {
+  name: 'App',
+  components: {
+    PokemonPage,
+}
+}
+</script>
+
+<style>
+#app {
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-top: 60px;
+  text-align: center;
+}
+</style>
---
+++ Vue/03-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Quién es este Pokemon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
