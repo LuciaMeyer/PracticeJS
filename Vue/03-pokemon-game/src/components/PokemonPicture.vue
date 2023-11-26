95574e0ed6e6337afafda95e4ee78c03e78a520a
LuciaMeyer <lucia@codice.dev>
2023-11-26 00:33:03 -0300

    finish poke-app

---
+++ Vue/03-pokemon-game/src/components/PokemonPicture.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="pokemon-container">
+		<img v-if="!showPokemon"
+			:src="imgSrc"
+			alt="pokemon"
+			class="hidden" />
+		<img v-else
+			class="fade-in"
+			:src="imgSrc"
+			alt="pokemon" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		pokemonId: {
+			type: Number,
+			required: true,
+		},
+		showPokemon: {
+			type: Boolean,
+			required: true,
+			default: false,
+		}
+	},
+	computed: {
+		imgSrc() {
+			return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.pokemon-container {
+	height: 250px;
+}
+img {
+	height: 200px;
+	/* position: absolute;
+	right: 43vw; */
+	user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	-webkit-user-drag: none;
+	-webkit-user-select: none;
+}
+.hidden {
+	filter: brightness(0);
+}
+</style>
--- Vue/03-pokemon-game/src/pages/PokemonPage.vue
+++ Vue/03-pokemon-game/src/pages/PokemonPage.vue
@@ -1,15 +1,67 @@
 <template>
-  <div>
-    <h1>Quién es este Pokemon</h1>
-  </div>
+	<div>
+		<h1 v-if="!pokemon">cargando...</h1>
+		<div v-else>
+			<h1>Quién es este Pokemon</h1>
+			<PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+			<PokemonOption :pokemons="pokemonArr" @selection="checkAnswer" />
+			<template  v-if="showAnswer">
+				<h2 class="fade-in">{{ msj }}</h2>
+				<button @click="newGame">Nuevo Juego</button>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
+import PokemonOption from "@/components/PokemonOption.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 
-}
+export default {
+	name: "PokemonPage",
+	components: {
+		PokemonOption,
+		PokemonPicture,
+	},
+	data() {
+		return {
+			pokemonArr: [],
+			pokemon: null,
+			showPokemon: false,
+			showAnswer: false,
+			msj: "",
+		};
+	},
+	methods: {
+		async mixPokemonArr() {
+			this.pokemonArr = await getPokemonOptions();
+			const randomNum = Math.floor(Math.random() * 4);
+			this.pokemon = this.pokemonArr[randomNum];
+		},
+		checkAnswer(pokeId) {
+			this.showPokemon = true;
+			this.showAnswer = true;
+			if (pokeId === this.pokemon.id) {
+				this.msj = `Correcto! es ${this.pokemon.name}`;
+			} else {
+				this.msj = `Ups! era ${this.pokemon.name}`;
+			}
+		},
+		newGame() {
+			this.showPokemon = false
+			this.showAnswer = false
+			this.pokemonArr = []
+			this.pokemon = null
+			this.mixPokemonArr()
+		}
+	},
+	// life cycle
+	mounted() {
+		this.mixPokemonArr();
+	},
+};
 </script>
 
 <style>
-
 </style>
